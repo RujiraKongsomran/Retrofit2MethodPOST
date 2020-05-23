5595cfcfcb891323065984d9557053bb6d0d0d86
Rujira Kongsomran <biwberry@outlook.com>
2020-05-23 16:10:15 +0700

    Create Model

---
+++ app/src/main/java/com/rujirakongsomran/retrofit2methodpost/Model/Response.java
@@ -0,0 +1,22 @@
+package com.rujirakongsomran.retrofit2methodpost.Model;
+
+public class Response {
+    private String status;
+    private Data data;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+}

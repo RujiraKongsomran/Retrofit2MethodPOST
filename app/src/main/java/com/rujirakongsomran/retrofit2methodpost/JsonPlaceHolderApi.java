28a63bb182edf5d9d65ece5ccd83f2397d1530f0
Rujira Kongsomran <biwberry@outlook.com>
2020-05-23 16:56:20 +0700

    Create Interface

---
+++ app/src/main/java/com/rujirakongsomran/retrofit2methodpost/JsonPlaceHolderApi.java
@@ -0,0 +1,14 @@
+package com.rujirakongsomran.retrofit2methodpost;
+
+import com.rujirakongsomran.retrofit2methodpost.Model.Request;
+import com.rujirakongsomran.retrofit2methodpost.Model.Response;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface JsonPlaceHolderApi {
+
+    @POST("api/v1/create")
+    Call<Response> postBlueDot(@Body Request request);
+}
